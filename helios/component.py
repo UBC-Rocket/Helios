08a1cab9f23b149a10a6e5ac90107cd0e9117a9d
Edward Li <radioactivehydra@gmail.com>
2024-02-07 10:12:36 -0800

    Added Additional Comments

--- helios/component.py
+++ helios/component.py
@@ -1,66 +1,175 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+
 if TYPE_CHECKING:
     from helios.helios import Helios
     from helios.components.base import ComponentBase
 
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Optional
+from typing_extensions import override
 from multiprocessing import Process
 
 
 class AbstractComponent(ABC):
+    """Abstract class for a component.
+
+    This is a common interface for all components in Helios, including
+    component groups and component managers.
+
+    Component groups contain zero or more other components, and are used to
+    organize components into a tree structure. Component managers are used to
+    manage the lifecycle of a component, including starting, stopping, and
+    killing the component.
+
+    Attributes:
+        name (str): The name of the component.
+        parent (ComponentGroup, optional): The parent of the current component.
+            Defaults to None. If None, there is not parent
+    """
+
     def __init__(self, name: str, parent: Optional[ComponentGroup] = None):
+        """Creates a simple component object.
+
+        Args:
+            name (str): The name of the component.
+            parent (ComponentGroup, optional): The parent of the current component.
+                Defaults to None. If None, there is not parent
+        """
         self.name: str = name
         self.parent: Optional[ComponentGroup] = parent
 
+    @abstractmethod
     def get_path(self) -> str:
+        """Returns the full path of the component in string form.
+
+        For example, if the component is named "test" and is a child of a component named "parent",
+        the return value would be "parent.test". If the component has no parent, the return value would be "test".
+
+        The path will contain all parent components all the way up to the root component,
+        or the first component that has no parent.
+
+        Returns:
+            str: The full path of the component
+        """
         raise NotImplementedError()
 
+    @abstractmethod
     def print_tree(self, last=True, header=''):
+        """Prints a formatted tree of the component and its children.
+
+        An example output is:
+        └──[basic_test] (4 children)
+            ├──<test_producer: self.running=False>
+            │  └──(<helios.component.PyComponent object at 0x7fec375e3cd0>)
+            ├──<test_consumer: self.running=False>
+            │  └──(<helios.component.PyComponent object at 0x7fec375e3d90>)
+            ├──<test_subscriber: self.running=False>
+            │  └──(<helios.component.PyComponent object at 0x7fec375e3e50>)
+            └──[test_group] (2 children)
+                ├──<test_producer_two: self.running=False>
+                │  └──(<helios.component.PyComponent object at 0x7fec375e3f70>)
+                └──<test_consumer_two: self.running=False>
+                    └──(<helios.component.PyComponent object at 0x7fec37638070>)
+
+        Args:
+            last (bool, optional): Is this the last row in the print? Defaults to True.
+            header (str, optional): Header to print before formatting. Defaults to ''.
+        """
         raise NotImplementedError()
 
 
 class ComponentGroup(AbstractComponent):
+    """Component group class.
+
+    This class is used to organize components into a tree structure. It can contain
+    zero or more other components, including other component groups and component managers.
+
+    Attributes:
+        components (dict[str, AbstractComponent]): A dictionary of components, where the key is the name of the component
+            and the value is the component object.
+    """
+
+    @override
     def __init__(self, name: str, parent: Optional[ComponentGroup] = None):
         super().__init__(name, parent)
         self.components: dict[str, AbstractComponent] = {}
 
     def create_component_group(self, name: str) -> ComponentGroup:
+        """Creates a new component group and adds it to the components dictionary.
+
+        Args:
+            name (str): Name of the component group to create.
+
+        Raises:
+            ValueError: If a component with the same name already exists.
+
+        Returns:
+            ComponentGroup: The newly created component group.
+        """
+        # Check if component with name already exists
         if name in self.components:
             raise ValueError(f"Component with name {name} already exists")
 
+        # Create the component group and add it to the components dictionary
         group: ComponentGroup = ComponentGroup(name, parent=self)
         self.components[name] = group
         return group
 
     def add_component(self, component: AbstractComponent):
+        """Adds a component to the component group.
+
+        Args:
+            component (AbstractComponent): The component to add.
+
+        Raises:
+            ValueError: If a component with the same name already exists.
+            ValueError: If the component is not a valid component.
+        """
+        # Check if component with name already exists
         if component.name in self.components:
             raise ValueError(f"Component with name {component.name} in group {self.get_path()} already exists")
 
+        # Check what type of component it is
         if isinstance(component, ComponentGroup):
+            # If it's a component group, add it to the components dictionary
             self.components[component.name] = component
-
         elif isinstance(component, AbstractComponentManager):
+            # If it's a component manager, add it to the components dictionary
+            # and set the parent of the component to the current component group
             self.components[component.name] = component
             component.parent = self
-
         else:
+            # This should not happen, but raise an error if the component is not valid
             raise ValueError(f"Component with name {component.name} is not a valid component")
 
+    def start_all(self, server: Helios):
+        """Recursively starts all components in the component group.
+
+        Args:
+            server (Helios): The Helios server instance. Used to pass to the start method of the components.
+
+        Raises:
+            ValueError: If the component is not a valid component.
+        """
+        # Loop through all components and start them
+        for c in self.components.values():
+            if isinstance(c, AbstractComponentManager):
+                # If the component is a component manager, start it
+                c.start(server)
+            elif isinstance(c, ComponentGroup):
+                # If the component is a component group, recursively start all of its children
+                c.start_all(server)
+            else:
+                # This should not happen, but raise an error if the component is not valid
+                raise ValueError(f"Component {c.get_path()} is not a valid component")
+
+    @override
     def get_path(self) -> str:
         return f"{self.parent.get_path()}.{self.name}" if self.parent else self.name
 
-    def start_all(self, server: Helios):
-        for c in self.components.values():
-            if isinstance(c, AbstractComponentManager):
-                c.start(server)
-            elif isinstance(c, ComponentGroup):
-                c.start_all(server)
-            else:
-                raise ValueError(f"Component {c.get_path()} is not a valid component")
-
+    @override
     def print_tree(self, last=True, header=''):
         if not self.parent:
             print("[HELIOS CORE]")
@@ -75,31 +184,63 @@ class ComponentGroup(AbstractComponent):
 
 
 class AbstractComponentManager(AbstractComponent, ABC):
+    """Abstract class for a component manager.
+
+    This class is used to manage the lifecycle of a component, including starting, stopping, and killing the component.
+
+    Attributes:
+        running (bool): Is the component currently running?
+    """
+    @override
     def __init__(self, name: str, parent: Optional[ComponentGroup] = None):
         super().__init__(name, parent)
         self.running: bool = False
 
+    @abstractmethod
     def start(self, server: Helios):
+        """Starts the component.
+
+        Args:
+            server (Helios): The Helios server instance. Used to pass to the start method of the components.
+
+        Raises:
+            ValueError: If the component is already running.
+        """
         raise NotImplementedError()
 
-    def stop(self, server: Helios):
+    @abstractmethod
+    def stop(self):
+        """Stops the component.
+
+        Raises:
+            ValueError: If the component is not running.
+        """
         raise NotImplementedError()
 
-    def kill(self, server: Helios):
+    @abstractmethod
+    def kill(self):
+        """Kills the component.
+
+        Raises:
+            ValueError: If the component is not running.
+        """
         raise NotImplementedError()
 
 
 class PyComponent(AbstractComponentManager):
-    def __init__(self, name: str, component_object: ComponentBase):
-        super().__init__(name)
+    def __init__(self, name: str, component_object: ComponentBase, parent: Optional[ComponentGroup] = None):
+        super().__init__(name, parent)
 
         self.component_object: ComponentBase = component_object
         self.process: Optional[Process] = None
 
+    @override
     def start(self, server: Helios):
+        # Check if component is already running
         if self.running:
             raise ValueError(f"Component {self.get_path()} is already running")
 
+        # Populate the name, path, and gRPC information for the component
         self.component_object.init_component(
             name=self.name,
             path=self.get_path(),
@@ -107,6 +248,7 @@ class PyComponent(AbstractComponentManager):
             grpc_port=server.grpc_port
         )
 
+        # Create the python process
         self.process = Process(
             target=self.component_object.run,
             args=self.component_object.launch_args,
@@ -114,12 +256,23 @@ class PyComponent(AbstractComponentManager):
             daemon=True
         )
 
+        # Start the process
         self.running = True
         self.process.start()
 
+    @override
+    def stop(self, server: Helios):
+        raise NotImplementedError()
+
+    @override
+    def kill(self, server: Helios):
+        raise NotImplementedError()
+
+    @override
     def get_path(self) -> str:
         return f"{self.parent.get_path()}.{self.name}" if self.parent else self.name
 
+    @override
     def print_tree(self, last=True, header=''):
         elbow = "└──"
         pipe = "│  "
--- helios/helios.py
+++ helios/helios.py
@@ -8,39 +8,76 @@ import grpc
 
 
 class Helios:
+    """High level server class for Helios.
+
+    Attributes:
+        base_name (str): Base name of the server.
+        friendly_name (str): Print-friendly name of the server.
+        grpc_host (int): Host of the gRPC server.
+        grpc_port (int): Port of the gRPC server.
+        component_tree (ComponentGroup): Root-level component group for all components.
+    """
     def __init__(
         self,
         base_name: str,
-        friendly_name: str,
+        friendly_name: Optional[str] = None,
         grpc_host: str = "[::]",
         grpc_port: int = 50051,
     ):
+        """Creates Helios server instance.
+
+        Args:
+            base_name (str): Base name of the server.
+            friendly_name (str): Print-friendly name of the server.
+            grpc_host (int, optional): The address for which to open a port. Defaults to "[::]".
+            grpc_port (int, optional): Integer port on which server will accept RPC requests. Defaults to 50051.
+        """
         # Argument-defined attributes
         self.base_name: str = base_name
+        self.friendly_name: str = friendly_name if friendly_name else base_name
         self.grpc_host: str = grpc_host
         self.grpc_port: int = grpc_port
-        self.friendly_name: Optional[str] = friendly_name
 
         # Other attributes
         self.component_tree: ComponentGroup = ComponentGroup(self.base_name)
 
     def get_component_tree(self) -> ComponentGroup:
-        # Return the component tree
+        """Returns the component tree.
+
+        Returns:
+            ComponentGroup: The base component group.
+        """
         return self.component_tree
 
-    def init_grpc_server(self):
+    def create_grpc_server(self) -> grpc.Server:
+        """Generates a gRPC server instance.
+
+        This method initializes the gRPC server and adds the protocol handler.
+
+        Returns:
+            grpc.Server: The gRPC server instance.
+        """
         # Create the gRPC server instance
-        self.grpc_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        grpc_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
         # Add protocol handler
-        add_HeliosProtocolServicer_to_server(HeliosProtocol(), self.grpc_server)
+        add_HeliosProtocolServicer_to_server(HeliosProtocol(), grpc_server)
 
         # Add the bind info
-        self.grpc_server.add_insecure_port(f"{self.grpc_host}:{self.grpc_port}")
+        grpc_server.add_insecure_port(f"{self.grpc_host}:{self.grpc_port}")
+
+        return grpc_server
 
     def start(self):
+        """Starts the Helios server.
+
+        This creates, initializes, and starts the gRPC server,
+        and recursively starts all components.
+
+        This method will block until the gRPC server is terminated.
+        """
         # Initialize the gRPC server
-        self.init_grpc_server()
+        self.grpc_server = self.create_grpc_server()
 
         # Start the gRPC server
         self.grpc_server.start()
