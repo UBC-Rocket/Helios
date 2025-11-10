d901677c2273899f7f67ba771ef938d32d4c6fdd
Edward Li <jiaxi.edwardli@gmail.com>
2025-11-10 12:09:03 -0800

    Add initial protobuf integration

--- cmd/helios/main.go
+++ cmd/helios/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "helios/internal/test"
+import (
+	"fmt"
+	pb "helios/generated/github.com/protocolbuffers/protobuf/examples/go/tutorialpb"
+	"helios/internal/test"
+
+	"google.golang.org/protobuf/proto"
+)
 
 func main() {
 	test.HelloWorld()
+
+	person := &pb.Person{
+		Name:  "John Doe",
+		Id:    1234567890,
+		Email: "john.doe@example.com",
+	}
+
+	fmt.Println(person.String())
+
+	data, err := proto.Marshal(person)
+	if err != nil {
+		fmt.Printf("Failed to marshal person: %v", err)
+	}
+
+	fmt.Println(data)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module helios
 
 go 1.24.9
+
+require google.golang.org/protobuf v1.36.10
